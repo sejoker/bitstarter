b176ec1866b48b65392e71244ee07774668bb5d8
Ubuntu <ubuntu@ip-172-31-11-244.eu-west-1.compute.internal>
2013-07-22 20:35:21 +0000

    sdfasf

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var buffer =  fs.readFileSync('index.html'); 
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
